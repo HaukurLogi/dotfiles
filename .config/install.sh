7f49aca28ed3306129d8f860a410948735e7ba92
hauksi logisti <hauksilogisti@pop-os.localdomain>
2021-02-20 15:30:35 +0000

    fix installation script

--- .config/install.sh
+++ .config/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xeo pipefail
+set -xe
 
 git clone --bare https://github.com/HaukurLogi/dotfiles.git $HOME/.dotfiles
 
@@ -9,5 +9,5 @@ function config {
 
 mkdir -p .dotfiles-backup
 config checkout || config checkout 2>&1 | egrep "^\s+" | awk {'print $1'} | xargs -I{} bash -c 'mkdir -p `dirname .dotfiles-backup/{}` && mv {} .dotfiles-backup/{}'
-
+config checkout
 config config --local status.showUntrackedFiles no
