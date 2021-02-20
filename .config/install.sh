92d40b0ff44e8d7da5d65744cbff4e11b89c6f21
hauksi logisti <hauksilogisti@pop-os.localdomain>
2021-02-20 14:29:46 +0000

    Add installation script

---
+++ .config/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xeo pipefail
+
+git clone --bare https://github.com/HaukurLogi/dotfiles.git $HOME/.dotfiles
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+mkdir -p .dotfiles-backup
+config checkout
+if [ $? = 0 ]; then
+    echo "Checked out config.";
+else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+fi;
+
+config checkout
+config config --local status.showUntrackedFiles no
---
+++ README.md
@@ -0,0 +1,4 @@
+Run the following:
+```
+curl -Lks https://raw.githubusercontent.com/HaukurLogi/dotfiles/master/.config/install.sh | bash
+```
