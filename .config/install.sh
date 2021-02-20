7fd4df734f1a660b5a8295b93db14d1519c5fbfb
hauksi logisti <hauksilogisti@pop-os.localdomain>
2021-02-20 15:01:31 +0000

    fix installation script

--- .config/install.sh
+++ .config/install.sh
@@ -8,13 +8,6 @@ function config {
 }
 
 mkdir -p .dotfiles-backup
-config checkout
-if [ $? = 0 ]; then
-    echo "Checked out config.";
-else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
-fi;
+config checkout || config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} bash -c 'mkdir -p `dirname .dotfiles-backup/{}` && mv {} .dotfiles-backup/{}'
 
-config checkout
 config config --local status.showUntrackedFiles no
