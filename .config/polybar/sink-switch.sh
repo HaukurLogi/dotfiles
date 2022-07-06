d33d9134b830f4bf19942efca9245016dab913af
HaukurLogi <haukurlogi2008@gmail.com>
2022-07-06 17:32:59 +0000

    Fixing

---
+++ .config/polybar/sink-switch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+arg="${1:-}"
+case "$arg" in
+  --speakers)
+    CARD="Speakers"
+    SINK="alsa_output.usb-Bose_Corporation_Bose_USB_Audio-00.analog-surround-51"
+    pacmd set-default-sink "$SINK"
+    pacmd list-sink-inputs | grep index | while read line; do
+    pacmd move-sink-input `echo $line | cut -f2 -d' '` "$SINK"
+    done
+    ;;
+  --headphones)
+    CARD="Headphones"
+    SINK="alsa_output.pci-0000_00_1b.0.analog-stereo"
+    pacmd set-default-sink "$SINK"
+    pacmd list-sink-inputs | grep index | while read line; do
+    pacmd move-sink-input `echo $line | cut -f2 -d' '` "$SINK"
+    done
+    ;;
+    *)
+echo "Headphones/Speakers"
+    ;;
+esac
