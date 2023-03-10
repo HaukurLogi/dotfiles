7157f48b9ca866c766fe0f1944d8c79e2e1d3b63
haukurlogi <haukurlogi2008@gmail.com>
2023-03-10 09:24:41 +0000

    made new keybind in nvim and made mac and linux wallpapers seprate

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -41,7 +41,9 @@ keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 -- Better paste
 keymap("v", "p", '"_dP', opts)
 
--- Insert --
+-- Save --
+keymap("n", "<C-S>", ":w<CR>", opts)
+
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 
