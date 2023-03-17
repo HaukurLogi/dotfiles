c785c07d08ecefd86d3ce953de457388c3037872
haukurlogi <haukurlogi2008@gmail.com>
2023-03-17 16:22:24 +0000

    changed alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -5,4 +5,4 @@ alias diff="diff --color"
 # Config management alias; see the following for details:
 # https://developer.atlassian.com/blog/2016/02/best-way-to-store-dotflies-git-bare-repo/
 alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias pp='/usr/bin/git --git-dir=$HOME/python-projects/.python-projects --work-tree=$HOME/python-projects'
+alias pyp='/usr/bin/git --git-dir=$HOME/python-projects/.python-projects --work-tree=$HOME/python-projects'
