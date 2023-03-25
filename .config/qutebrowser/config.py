8d2798b408346b8954970e8974feae8a168a5808
haukurlogi <haukurlogi2008@gmail.com>
2023-03-25 19:37:59 +0000

    updated config.py and made default webpage

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,3 +1,7 @@
 config.load_autoconfig()
+
+# Darkmode
 config.set("colors.webpage.darkmode.enabled", True)
+
+# Starting page
 c.url.start_pages = 'file:///home/haukur/.config/qutebrowser/default_page/index.html'
