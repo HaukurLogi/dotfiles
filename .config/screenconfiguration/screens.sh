86b6c706014747587362355ec5c967ccffa463fa
haukurlogi <haukurlogi2008@gmail.com>
2023-06-11 10:22:17 +0000

    added screenconfiguration

---
+++ .config/screenconfiguration/screens.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off
