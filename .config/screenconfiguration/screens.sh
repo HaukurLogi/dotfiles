15f47eb6072ef9155c24be7744fd6f68c4618f7e
haukurlogi <haukurlogi2008@gmail.com>
2023-06-13 17:04:03 +0000

    fixed monitor issue at mothers house

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --primary --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --off --output DP-5 --off
+xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 1280x720 --pos 0x0 --rotate left --output DP-4 --primary --mode 1920x1080 --rate 240.00 --pos 720x492 --rotate normal --output DP-5 --off
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal
