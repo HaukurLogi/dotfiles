466690f2cf75da0b1583d7c411ca508f1d638ec2
HaukurLogi <haukurlogi2008@gmail.com>
2023-06-23 22:12:11 +0000

    removed wallpaper from i3 config

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 1280x720 --pos 0x0 --rotate left --output DP-4 --primary --mode 1920x1080 --rate 240.00 --pos 720x492 --rotate normal --output DP-5 --off
+xrandr --output DP-4 --primary --mode 1920x1080 --pos 720x0 --rotate normal --rate 240.00 --output HDMI-0 --mode 1280x720 --left-of DP-4 --pos 0x0 --rotate left --rate 60.00
 xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal
--- .zshrc
+++ .zshrc
@@ -113,4 +113,3 @@ pfetch
 
 # Generated for envman. Do not edit.
 [ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
-
