9398a360272d4761536845c57461ccec865f1fe4
haukurlogi <haukurlogi2008@gmail.com>
2023-07-29 13:55:37 +0000

    fixed plane-runway-calculator.py

--- python/plane-runway-calculator.py
+++ python/plane-runway-calculator.py
@@ -1,17 +1,14 @@
+timePassed = 0
+
+planeVelocity = 70 # The planes velocity needed to takeoff m/s
+planeAcceleration = 30 # The planes takeoff acceleration meters
+
+minimumTakeoffRunway = 0 
 
 
-planeSpeed = 0 # Plane's Current Speed Meters Per Second
-planeMass = 1134 # Plane's Current Kilo Newtons
-planeAcceleration = (planeMass / planeMass) ** 2 # Plane's Current Acceleration Meters Per Second
+# Calculations
+timePassed = planeVelocity / planeAcceleration 
+planeAcceleration = planeVelocity / timePassed 
+minimumTakeoffRunway = 1 / 2 * (planeAcceleration * timePassed) ** 2 
+print(minimumTakeoffRunway)
 
-runwayLengthTakeoff = 0 # Required Runway Length for Takeoff
-runwayLengthLanding = 0 # Required Runway Length for Landing
-
-
-def minimumRunwayTakeoff(speed, acceleration):
-    while True:
-        speed += acceleration
-        if speed >= 66:
-            print("{:.0f} meters needed for takeoff.".format(speed ** 2 / (2 * acceleration)))
-            break
-minimumRunwayTakeoff(planeSpeed, planeAcceleration)
