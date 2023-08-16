8d35e540251b0ed404db21c81ea99d67fa9a7c07
HaukurLogi <haukurlogi2008@gmail.com>
2023-08-16 17:43:13 +0000

    changed screenconfiguration

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-xrandr --output DP-4 --primary --mode 1920x1080 --rotate normal --rate 240.00 
+xrandr --output DP-0 --primary --mode 1920x1080 --rotate normal --rate 240.00 
 xrandr --output HDMI-0 --mode 1280x720 --left-of DP-4 --pos 0x0 --rotate left --rate 60.00
 xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal
