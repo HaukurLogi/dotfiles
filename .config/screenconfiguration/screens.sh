2c9317873cd64d9704dfe2758a0a68a64f609044
HaukurLogi <haukurlogi2008@gmail.com>
2023-09-01 08:20:35 +0000

    deleted i3 + kde

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output DP-0 --primary --mode 1920x1080 --rotate normal --rate 240.00 
-xrandr --output HDMI-0 --mode 1280x720 --left-of DP-4 --pos 0x0 --rotate left --rate 60.00
-xrandr --output DP-0 --mode 2560x1440 --rate 144.00 --pos 0x0 --rotate normal
+
+xrandr --output DP-0 --mode 1920x1080 --rate 240.00 # Sjávargrund 
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00 # Eikarás 
+xrandr --output eDP-1 --mode 3024x1890 --scale 0.85x0.85 # MacBook Pro 
