85df85e7044506e2b4035b5cb8a3907525aac8b4
haukurlogi <haukurlogi2008@gmail.com>
2023-09-04 18:56:58 +0000

    made rofi a little less transparent and changed dp in screens.sh

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 xrandr --output DP-0 --mode 1920x1080 --rate 240.00 # Mamma 
-xrandr --output DP-0 --mode 2560x1440 --rate 144.00 # Pabbi 
+xrandr --output DP-2 --mode 2560x1440 --rate 144.00 # Pabbi 
 xrandr --output eDP-1 --mode 3024x1890 --dpi 192 # MacBook 
