6cdce875374830e54fbc9b303eb3d4c3cbcbc799
HaukurLogi <haukurlogi2008@gmail.com>
2023-09-20 10:55:31 +0000

    made losing possible in sudoku.py

--- python/sudoku.py
+++ python/sudoku.py
@@ -23,7 +23,6 @@ def makeValidTable(grid):
 
     for row in range(0,9):
         appendedNumbersInRow.clear()
-        appendedNumbersInColumn.clear()
         for column in grid[row]:
             skipColumn = bool(randint(0,1))
             randomNumber = randint(1,9)
@@ -42,13 +41,28 @@ def placeNumber(level):
     y = int(input("Y : ")) - 1
     number = int(input("Number : "))
 
-    level[x][y] = number # Places number in level
+    level[y][x] = number # Places number in level
+
+    seenX = []
+    seenY = []
+
+    for row in range(0,9):
+        for column, number in enumerate(level[row]):
+            if number != 0:
+                if number not in seenX: # Check Row
+                    seenX.append(number)
+                if (column, number) not in seenY: # Check Column
+                    seenY.append((column, number))
+                else:
+                    print("You lost!")
+                    return 0
 
     printLevel(level)
     return level # Return edited level
 
 level = makeValidTable(defaultGrid) # Create valid sudoku table
 
-
 while True:
     level = placeNumber(level)
+    if level == 0:
+        break
