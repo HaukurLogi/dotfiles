8231db789b96715ea9e697f122a8355e886e5983
HaukurLogi <haukurlogi2008@gmail.com>
2023-10-08 11:52:37 +0000

    added codepython alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,7 +1,8 @@
-alias ls="ls --color"
-alias grep="grep --color"
+alias clearall="clear"
+alias codepython"cd ~/python && code . && exit"
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias diff="diff --color"
+alias grep="grep --color"
+alias ls="ls --color"
 alias poff="poweroff"
 alias plear="clear && neofetch"
-alias clearall="clear"
-alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
