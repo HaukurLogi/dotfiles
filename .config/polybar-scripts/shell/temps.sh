72f69a29bcd34e7f621d5d8ae13111cf614eaea0
haukurlogi <haukurlogi2008@gmail.com>
2023-11-03 12:35:07 +0000

    made polybar temps better

--- .config/polybar-scripts/shell/temps.sh
+++ .config/polybar-scripts/shell/temps.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-printf "`echo "CPU-TEM: "``sensors | grep "Tctl" | tr -d '+' | awk '{print $2}'`  `echo "GPU-TEM: "` `sensors | grep "edge" | tr -d '+' | awk '{print $2}'`"
+CPU="Tctl"
+GPU="edge"
+
+CPU_COMMAND=$(sensors | grep $CPU | tr -d '+' | awk {'print $2'})
+GPU_COMMAND=$(sensors | grep $GPU | tr -d '+' | awk {'print $2'})
+
+if echo $COMMAND1 && echo $COMMAND2
+then 
+    printf "`echo "CPU-TEM: "``$COMMAND1`  `echo "GPU-TEM: "` `$COMMAND2`"
+fi 
+
