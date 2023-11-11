424f0d49b277dc561eac4c2ee11ec863a3a69187
HaukurLogi <haukurlogi2008@gmail.com>
2023-11-11 09:29:05 +0000

    fixed both polybar custom scripts not working; typos and bugs

--- .config/polybar-scripts/shell/gpu.sh
+++ .config/polybar-scripts/shell/gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-GPU_FILE=/sys/class/drm/card0/device/gpu_busy_percent
+GPU_FILE=/sys/class/drm/card1/device/gpu_busy_percent
 
 if test -f $GPU_FILE; then
   printf "`cat $GPU_FILE``echo "%%"`"
--- .config/polybar-scripts/shell/temps.sh
+++ .config/polybar-scripts/shell/temps.sh
@@ -6,7 +6,7 @@ GPU_SENSOR_NAME="edge"
 CPU_COMMAND=$(sensors | grep $CPU_SENSOR_NAME | tr -d '+' | awk {'print $2'})
 GPU_COMMAND=$(sensors | grep $GPU_SENSOR_NAME | tr -d '+' | awk {'print $2'})
 
-if echo $COMMAND1 && echo $COMMAND2; then 
-    printf "`echo "CPU-TEM: "``$COMMAND1`  `echo "GPU-TEM: "` `$COMMAND2`"
+if echo $CPU_COMMAND > /dev/null && echo $GPU_COMMAND > /dev/null; then 
+  printf "`echo "CPU-TEM: "``echo $CPU_COMMAND`  `echo "GPU-TEM: "` `echo $GPU_COMMAND`"
 fi 
 
