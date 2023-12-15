aae1e79eaaec5b573b1092075a9c4c89094d717e
haukurlogi <haukurlogi2008@gmail.com>
2023-12-15 11:51:35 +0000

    completed triangle block, fixed block spawning issue by adding copy library, fixed comments

--- python/dot.py
+++ python/dot.py
@@ -1,8 +1,11 @@
+import copy
 import random
 import time
 
+
 GRID_WIDTH = 10
 GRID_HEIGHT = 20
+GRID_TOP = 0
 GRID_MIDDLE = int(GRID_WIDTH / 2)
 
 
@@ -10,12 +13,20 @@ ACTIVE_CELL = "#"
 EMPTY_CELL = " "
 PLACED_CELL = "X"
 
+
 SLEEP_TIME = 0.1
 
+
 BLOCKS = [
-    [[[0, 0], [1, 0], [2, 0], [3, 0]], [[1, 0], [1, 1], [1, 2], [1, 3]]], # Straight block
-    [[[0, 1], [1, 0], [1, 1], [1, 2]], [[0, 1], [1, 0], [1, 1], [1, 2]]]  # Triangle block
+    [[[GRID_TOP, GRID_MIDDLE], [GRID_TOP + 1, GRID_MIDDLE], [GRID_TOP + 2, GRID_MIDDLE], [GRID_TOP + 3, GRID_MIDDLE]], 
+     [[GRID_TOP, GRID_MIDDLE], [GRID_TOP, GRID_MIDDLE + 1], [GRID_TOP, GRID_MIDDLE + 2], [GRID_TOP,  GRID_MIDDLE + 3]]], # Straight block
+   
+    [[[GRID_TOP, GRID_MIDDLE], [GRID_TOP + 1, GRID_MIDDLE + 1], [GRID_TOP + 1, GRID_MIDDLE], [GRID_TOP + 2, GRID_MIDDLE]], 
+     [[GRID_TOP, GRID_MIDDLE], [GRID_TOP + 1, GRID_MIDDLE + 1], [GRID_TOP, GRID_MIDDLE + 1], [GRID_TOP, GRID_MIDDLE + 2]],
+     [[GRID_TOP, GRID_MIDDLE + 1], [GRID_TOP + 1, GRID_MIDDLE], [GRID_TOP + 1, GRID_MIDDLE + 1], [GRID_TOP + 1, GRID_MIDDLE + 2]],
+     [[GRID_TOP, GRID_MIDDLE + 1], [GRID_TOP + 1, GRID_MIDDLE], [GRID_TOP + 1, GRID_MIDDLE + 1], [GRID_TOP + 2, GRID_MIDDLE + 1]]]  # Triangle block
 ]                                                                          
+
 
 def initialize_grid():
     return [[EMPTY_CELL] * GRID_WIDTH for _ in range(GRID_HEIGHT)] # Create and initialize the playable grid
@@ -29,9 +40,7 @@ def clear_current_position(grid):
 def print_grid(grid): # Print the current grid
     # Printing Grid
     for row in grid:
-        print(" ".join(row))
-
-    
+        print(EMPTY_CELL.join(row))
 
 def update_grid(grid, value, coordinates):
     clear_current_position(grid) # Clear the grid before placing the next cell
@@ -52,25 +61,23 @@ def move_auto_down(grid, placed_coordinates, coordinates, delay):
         time.sleep(delay)  # Block goes down every 'delay' seconds
         update_grid(grid, ACTIVE_CELL, coordinates)
 
-        # Check if the block has reached the bottom
+        # Check if the block has reached the bottom or a placed block
         if can_move_down(grid, coordinates):
             for row in range(len(coordinates)):
-                coordinates[row][0] += 1 # Moved block down if can_move_down returns false
+                coordinates[row][0] += 1 # Moved block down if can_move_down returns true
         else:
             placed_coordinates.append(coordinates) # Append the placed cells to placed coordinates array
             update_grid(grid, PLACED_CELL, coordinates) # Update the block with placed cells
-            main(grid, placed_coordinates) # Spawn new block
-            break
-        
+            main(grid, placed_coordinates) # Spawn new block        
 
 def main(grid, placed_coordinates):
     random_block = random.randint(0, len(BLOCKS) - 1)
-    block_orientation = 0  # for every 1 is a 90-degree turn
-    current_block_coordinates = BLOCKS[random_block][block_orientation]
+    random_block_orientation = random.randint(0, len(BLOCKS[random_block]) - 1)  # for every 1 is a 90-degree turn
+    current_block_coordinates = copy.deepcopy(BLOCKS[random_block][random_block_orientation]) # Make a copy of the block to ensure the BLOCK variable doesn't change
 
     move_auto_down(grid, placed_coordinates, current_block_coordinates, SLEEP_TIME)
 
 if __name__ == "__main__":
-    grid = initialize_grid()
-    placed_block_coordinates = []
-    main(grid, placed_block_coordinates)
+    grid = initialize_grid() # At the start of the game create the grid
+    placed_block_coordinates = [] # Define placed block coordinates
+    main(grid, placed_block_coordinates) # Let the games begin!
