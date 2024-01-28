2343f62a67d38e98ba7e0e31c7967b435f22916a
HaukurLogi <haukurlogi2008@gmail.com>
2024-01-28 18:11:59 +0000

    made conky hud with quark wallpaper and changed mpvbg accordingly and added picom again

--- bin/mpvbg.sh
+++ bin/mpvbg.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
+# Uses xwinwrap to display given animated .gif in the center of the screen
 
-set -ex
-
-if [ "$#" -eq 0 ]; then
-    echo "Usage: mpvbg PATH [{w}x{h}+{x}+{y}]"
-    exit
-fi
-[ -f "$1" ] || exit
-
-pkill -9 xwinwrap || true
-
-if [ -n "$2" ]; then
-    xwin="xwinwrap -g "$2" -ni -fdt -sh rectangle -un -b -nf -ov -- "
-else
-    xwin="xwinwrap -ni -fdt -sh rectangle -un -b -nf -ov -fs -- "
+if [ $# -ne 1 ]; then
+echo 1>&2 Usage: $0 image.gif
+exit 1
 fi
 
-# --vo=xv  # less costly but laggy
-mpv="mpv -wid WID --no-config --keepaspect=no --loop \
-    --no-border --vd-lavc-fast --x11-bypass-compositor=no \
-    --gapless-audio=yes --hwdec=auto --really-quiet \
-    --name=mpvbg"
+# Get screen resolution
+SCRH=`xrandr | awk '/current/ { print $8 }'`
+SCRW=`xrandr | awk '/current/ { print $10 }' | sed 's/,//'`
 
-$xwin $mpv "$1" 2>&1 &
+# Get gif resolution
+IMGHW=`gifsicle --info $1 | awk '/logical/ { print $3 }'`
+IMGH=${IMGHW%x*}
+IMGW=${IMGHW#*x}
+
+# Calculate position
+POSH=$((($SCRH/2)-($IMGH/2)))
+POSW=$((($SCRW/2)-($IMGW/2)))
+
+xwinwrap -g ${IMGHW}+${POSH}+${POSW} -ov -ni -s -nf -b -sh circle -- gifview -w WID $1 -a
+
+exit 0
