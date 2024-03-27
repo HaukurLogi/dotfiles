4d5501ab74436d22ee94de1d842c1289cd26672a
haukurlogi <haukurlogi2008@gmail.com>
2024-03-27 15:20:49 +0000

    added screen to .config/screenconfiguration/screens.sh

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240.00 # Mamma 
-xrandr --output DP-0 --mode 2560x1440 --rate 144.00 # Pabbi 
+
+# Pabbi
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00
+xrandr --output HDMI-0 --mode 1920x1080 --rate 240.00
