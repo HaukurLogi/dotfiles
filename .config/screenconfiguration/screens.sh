114fb8feabb5111ffa070d9325ad1f8cfb0e0631
haukurlogi <haukurlogi2008@gmail.com>
2024-03-27 15:25:37 +0000

    added comment .config/screenconfiguration/screens.sh

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -3,5 +3,5 @@
 xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240.00 # Mamma 
 
 # Pabbi
-xrandr --output DP-0 --mode 2560x1440 --rate 144.00
-xrandr --output HDMI-0 --mode 1920x1080 --rate 240.00
+xrandr --output DP-0 --mode 2560x1440 --rate 144.00 # LG 
+xrandr --output HDMI-0 --mode 1920x1080 --rate 240.00 # BenQ
