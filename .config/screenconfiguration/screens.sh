2a9f6ea6cbb7519aae67af2cdedff1c108e8a0bd
HaukurLogi <haukurlogi2008@gmail.com>
2024-11-09 15:03:23 +0000

    changed name of workspaces in .config/screenconfiguration/screens.sh

--- .config/screenconfiguration/screens.sh
+++ .config/screenconfiguration/screens.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240.00 # Mamma 
+# Workspace 1
+xrandr --output DisplayPort-2 --mode 1920x1080 --rate 240.00
 
-# Pabbi
+# Workspace 2
 xrandr --output DP-0 --mode 2560x1440 --rate 144.00 # LG 
 xrandr --output HDMI-0 --mode 1920x1080 --rate 240.00 # BenQ
